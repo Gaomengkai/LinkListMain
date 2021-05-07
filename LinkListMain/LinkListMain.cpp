f2307feaa0ec446e4dde7d8a5768778e83c5399d
盟凯 高 <gaomengkai0@outlook.com>
2021-05-07 19:40:19 +0800

    Add project files.

---
+++ LinkListMain/LinkListMain.cpp
@@ -0,0 +1,12 @@
+﻿// LinkListMain.cpp : Defines the entry point for the application.
+//
+
+#include "LinkListMain.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
